74933d827a9d6c5432807159d7dce40b5d45af00
Fariba DK <faribacodes@gmail.com>
2021-01-22 10:55:34 -0800

    lunch

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan'); //nr
-const pg = require('pg');
-const client = new pg.Client('postgres://localhost/wikistack');
-const { layout } = require('./views/layout');
-const path = require('path'); //express nr
+//const pg = require('pg');
+// *const client = new pg.Client('postgres://localhost/wikistack');
+const layout = require('./views/layout');
+// const path = require('path'); //express.static -to join dirc name
 
 app.use(morgan('dev')); //invokes it - nr
-app.use(express.static(__dirname + './public')); 
+app.use(express.static('./public'));//app.use(express.static('public' 
 app.use(express.urlencoded({extended: false}));//has to be invoked middle w
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.get('/', (req, res) => {
     res.send(layout(''));
 });
-const PORT = 3000;
+app.listen(PORT, (req, res) => {
+    console.log('hi')
+    console.log('We are operating on 3001');
+});
+
 
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require('./app');
 const http = require('http'); //install http need to create server
 const server = http.createServer(app);
 
+const PORT = 3000;
 server.listen(PORT, (req, res) => {
     console.log('hi')
     console.log('We are operating on 3000');
